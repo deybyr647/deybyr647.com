07d1edd8fc7d84a682e3fcdede38126c54067e25
deybyr647 <deybyr647@gmail.com>
2026-02-08 12:38:39 -0500

    edited configs

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
 };
 
 export default nextConfig;
